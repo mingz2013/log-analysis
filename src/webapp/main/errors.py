efb9790576c88ff21b962d306ab00b4d23ca2ddc
mingz <305603665@qq.com>
2017-07-07 19:09:48 +0800

    init

--- src/webapp/main/errors.py
+++ src/webapp/main/errors.py
@@ -6,11 +6,6 @@ from flask import render_template
 from . import main
 
 
-@main.app_errorhandler(500)
-def error_500(e):
-    return render_template('errors/500.html'), 500
-
-
 @main.app_errorhandler(403)
 def error_403(error):
     # app.logger.error("403")
@@ -23,3 +18,15 @@ def error_404(error):
     # app.logger.error("404")
     return render_template('errors/404.html'), 404
     # return '404', 404
+
+
+@main.app_errorhandler(405)
+def error_405(error):
+    # app.logger.error("405")
+    return render_template('errors/405.html'), 405
+    # return '405', 405
+
+
+@main.app_errorhandler(500)
+def error_500(error):
+    return render_template('errors/500.html'), 500
