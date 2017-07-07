f69c6f700fc07e571b4e3498745120f0bf124a75
mingz <305603665@qq.com>
2017-07-07 11:13:42 +0800

    init

--- analysis/analysis_crontab.sh
+++ analysis/analysis_crontab.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+
+cd /home/zhaojingming/apps/log_analysis/analysis/
 find /home/log37/10.* -name GT712*`date +"%Y_%m_%d" --date='1 days ago'` | xargs grep '===analysis===' | awk -F '===analysis===' '{print $2}' > tmp/tmp_`date +"%Y_%m_%d" --date='1 days ago'`.json
 pypy src/analysis.py
