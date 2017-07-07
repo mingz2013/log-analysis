73d3100b88dbb1e9d6179a60b0297628071e2721
mingz <305603665@qq.com>
2017-07-07 16:23:59 +0800

    init

--- src/webapp/main/analysis.py
+++ src/webapp/main/analysis.py
@@ -2,8 +2,10 @@
 __author__ = 'zhaojm'
 
 from . import main
+from flask import render_template
 
 
 @main.route('/', methods=['GET'])
 def index():
-    return "index"
+    # return "index"
+    return render_template("index.html")
