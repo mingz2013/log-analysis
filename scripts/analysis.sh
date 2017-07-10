f7004d94ca32378d4b9adc140abef45f43083cc6
mingz <305603665@qq.com>
2017-07-10 11:32:42 +0800

    init

--- scripts/analysis.sh
+++ scripts/analysis.sh
@@ -4,5 +4,6 @@ last_day=`date +"%Y_%m_%d" --date='1 days ago'`
 echo "========================$last_day=====begin=========================="
 cd /home/zhaojingming/apps/log_analysis/
 find /home/log37/10.* -name GT712*$last_day | xargs grep '===analysis===' | awk -F '===analysis===' '{print $2}' > tmp/tmp_$last_day.json
+find /home/log37/10.* -name GT711*$last_day | xargs grep '===analysis===' | awk -F '===analysis===' '{print $2}' >> tmp/tmp_$last_day.json
 pypy src/analysis/setup.py
 echo "========================$last_day=====end============================"
