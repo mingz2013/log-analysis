705dcdbf69e08b2a08d0a00b387706fa939c9097
mingz <305603665@qq.com>
2017-07-12 10:18:51 +0800

    init

--- src/analysis/table_config.py
+++ src/analysis/table_config.py
@@ -8,7 +8,24 @@ import json
 
 # 读取712配置文件
 j_712_obj = json.load(open('config/712.json'))
-table_config = j_712_obj['create_table_config']
+table_config_712 = j_712_obj['create_table_config']
+
+j_711_obj = json.load(open('config/711.json'))
+table_config_711 = j_712_obj['create_table_config']
+
+table_config = {}
+
+for (k, v) in table_config_711.items():
+    if k not in table_config:
+        table_config[k] = v
+    else:
+        print "conflict k", k
+
+for (k, v) in table_config_712.items():
+    if k not in table_config:
+        table_config[k] = v
+    else:
+        print "conflict k", k
 
 
 def get_key_desc(play_mode, param_type):
