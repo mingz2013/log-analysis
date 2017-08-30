ae417e74f43db6437738e33d2393c1b058de6b3b
mingz <305603665@qq.com>
2017-08-30 10:49:18 +0800

    init

--- src/analysis/plugins/bigresult_tongji.py
+++ src/analysis/plugins/bigresult_tongji.py
@@ -16,15 +16,19 @@ def print_big_result(l):
         # print 'item', item
         scores = item['scores']
         play_mode = item['play_mode']
+        cur_round_count = item['cur_round_count']
         if play_mode not in result:
             result[play_mode] = {}
+        if cur_round_count not in result[play_mode]:
+            result[play_mode][cur_round_count] = {}
+
         if not scores:
             print 'warn, scores is None'
             continue
         for score in scores:
-            if score not in result[play_mode]:
-                result[play_mode][score] = 0
-            result[play_mode][score] += 1
+            if score not in result[play_mode][cur_round_count]:
+                result[play_mode][cur_round_count][score] = 0
+                result[play_mode][cur_round_count][score] += 1
     print json.dumps(result, sort_keys=True, ensure_ascii=False)
 
     return result
